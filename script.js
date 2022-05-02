f43639bc352202a4be4b1dd8c7613c4cc148cde9
Reece Vela <reecevela@outlook.com>
2022-05-02 15:51:10 -0500

    Remove horizontal scrolling with media query

--- script.js
+++ script.js
@@ -2,73 +2,21 @@ window.addEventListener('load', () => {
   document.querySelector('.body').classList.add('loaded');
 });
 
+const softSkillSection = document.querySelector('.soft-skill-list');
+const techSkillSection = document.querySelector('.tech-skill-list');
 
-const pageSections = document.querySelectorAll('.page-section')
+let softSkills = ['Production Management','Communication','Supervising Experience','Leadership'];
+let techSkills = ['HTML5','CSS','JavaScript','Git','GitHub','Linux OS'];
 
-var handlers = [];
-
-pageSections.forEach(element => {
-  handlers[element] = () => onBecomeVisible(pageSections[element], handlerEffect[element]);
+softSkills.forEach(skill => {
+  const listItem = document.createElement('li');
+  listItem.textContent = skill;
+  softSkillSection.appendChild(listItem);
 });
 
-handlers.forEach(element => {
-  if (window.addEventListener) {
-    addEventListener('DOMContentLoaded', handlers[element], false);
-    addEventListener('load', handlers[element], false);
-    addEventListener('scroll', handlers[element], false);
-    addEventListener('resize', handlers[element], false);
-  } else if (window.attachEvent)  {
-    attachEvent('onDOMContentLoaded', handlers[element]); // Internet Explorer 9+ :(
-    attachEvent('onload', handlers[element]);
-    attachEvent('onscroll', handlers[element]);
-    attachEvent('onresize', handlers[element]);
-  }
+
+techSkills.forEach(skill => {
+  const listItem = document.createElement('li');
+  listItem.textContent = skill;
+  techSkillSection.appendChild(listItem);
 });
-
-//var handler = onBecomeVisible(pageSections[2], handlerEffect(2));
-
-function handlerEffect (index) {
-  console.log(index);
-};
-
-/*
-if (window.addEventListener) {
-  addEventListener('DOMContentLoaded', handler, false);
-  addEventListener('load', handler, false);
-  addEventListener('scroll', handler, false);
-  addEventListener('resize', handler, false);
-} else if (window.attachEvent)  {
-  attachEvent('onDOMContentLoaded', handler); // Internet Explorer 9+ :(
-  attachEvent('onload', handler);
-  attachEvent('onscroll', handler);
-  attachEvent('onresize', handler);
-}*/
-
-function isElementEntirelyInViewport (el) {
-  let rect = el.getBoundingClientRect();
-  return rect.top > 0 && rect.bottom <= window.innerHeight;
-}
-
-function onBecomeVisible(el, callback) {
-  return function () {
-    var visible = isElementEntirelyInViewport(el);
-    if (visible) {
-      if (typeof callback == 'function') {
-          callback();
-      }
-    }
-  }
-}
-
-function onVisibilityChange(el, callback) {
-  var old_visible;
-  return function () {
-      var visible = isElementEntirelyInViewport(el);
-      if (visible != old_visible) {
-          old_visible = visible;
-          if (typeof callback == 'function') {
-              callback();
-          }
-      }
-  }
-}
