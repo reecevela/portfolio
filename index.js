bffa4498762515e237c1cd2bfcad673bb3a53a7f
reecevela <reecevela@outlook.com>
2023-03-20 11:03:26 -0500

    Start redesign

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
-const softSkillSection = document.querySelector('.soft-skill-list');
-const techSkillSection = document.querySelector('.tech-skill-list');
+const GITHUB_USERNAME = 'reecevela';
+const GITHUB_API_URL = `https://api.github.com/users/${GITHUB_USERNAME}/repos`;
+const PINNED_REPOS = ['Address-Maker', 'Weather', 'Social-Media-Dashboard', 'Editable-Resume', 'Binary-Search-Tree'];
 
-let softSkills = ['Problem Solving','Teamwork','Communication','Supervising Experience','Leadership'];
-let techSkills = ['HTML5','CSS','JavaScript','Git','GitHub','Linux OS'];
-
-softSkills.forEach(skill => {
-  const listItem = document.createElement('li');
-  listItem.textContent = skill;
-  softSkillSection.appendChild(listItem);
+document.addEventListener('DOMContentLoaded', () => {
+  loadProjects();
 });
 
 
-techSkills.forEach(skill => {
-  const listItem = document.createElement('li');
-  listItem.textContent = skill;
-  techSkillSection.appendChild(listItem);
-});
+async function loadProjects() {
+    const projectsContainer = document.querySelector('.projects');
+
+    try {
+        const repos = await fetchGithubRepos();
+        const pinnedRepos = repos.filter((repo) => PINNED_REPOS.includes(repo.name));
+        
+        pinnedRepos.forEach((repo) => {
+            const project = createProjectElement(repo);
+            projectsContainer.appendChild(project);
+        });
+    } catch (error) {
+        console.error('Failed to load projects from GitHub:', error);
+    }
+}
+
+async function fetchGithubRepos() {
+  const response = await fetch(GITHUB_API_URL);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch GitHub repos: ${response.statusText}`);
+  }
+
+  return await response.json();
+}
+
+function createProjectElement(repo) {
+  const project = document.createElement('div');
+  project.classList.add('project');
+
+  const title = document.createElement('h3');
+  title.textContent = repo.name;
+  project.appendChild(title);
+
+  const link = document.createElement('a');
+  link.href = repo.html_url;
+  link.target = '_blank';
+  link.innerHTML = `<i class="fab fa-github"></i> GitHub Repository`;
+  project.appendChild(link);
+
+  const description = document.createElement('p');
+  description.textContent = repo.description;
+  project.appendChild(description);
+
+  return project;
+}
